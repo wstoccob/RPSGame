687e9eeae430c1fbfa7a55a64a7c20562f59f798
wstoccob <alinur.seysekov@gmail.com>
2024-04-22 19:58:21 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+    }
+
+    public class RuleDefiner
+    {
+        public void DefineRulesForTable(string[] args)
+        {
+
+        }
+    }
+
+    public class TableGenerator
+    { 
+        public void GenerateTable(string[] args)
+        {
+
+        }
+    }
+
+
+
+}
